e2d618f4c41749eea61ef7edf687634bf0364a7e
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2022-08-07 17:11:41 +0430

    Creat Project

---
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Kanani.java
@@ -0,0 +1,14 @@
+package com.orbitsoft.teamorbitsoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kanani extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kanani);
+    }
+}
