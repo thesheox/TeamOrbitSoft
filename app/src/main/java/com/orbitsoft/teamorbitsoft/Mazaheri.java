7aa7137c62092d81106633e0eb12f298edd004e3
rmsod <rmsod9803@gmail.com>
2022-08-08 12:01:02 +0430

    1

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Mazaheri.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Mazaheri.java
@@ -10,5 +10,6 @@ public class Mazaheri extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mazaheri);
+        //salam
     }
 }
