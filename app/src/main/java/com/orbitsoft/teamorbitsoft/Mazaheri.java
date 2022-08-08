c1916a592c6bd624f5df42f57e9d34ceb3ae676d
rmsod <rmsod9803@gmail.com>
2022-08-08 21:41:27 +0430

    6

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Mazaheri.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Mazaheri.java
@@ -22,35 +22,38 @@ TextView jad;
 
     }
 
-    public void zarb(){
-        jad=findViewById(R.id.textView4);
+    public void zarb() {
+        jad = findViewById(R.id.textView4);
         String tv = "";
-        String st="";
-        int zar=1;
-        for(int i=1;i<=11;i++)
-        {
-            for(int j=1;j<=11;j++)
-            {
-                zar=(i-1)*(j-1);
-                if(i==1 && j>1){zar=j-1;}
-                if(i>1 && j==1){zar=i-1;}
-                if(i==1 && j==1)
-                {
-                    st="x     ";
+        String st = "";
+        int zar = 1;
+        for (int i = 1; i <= 11; i++) {
+            for (int j = 1; j <= 11; j++) {
+                zar = (i - 1) * (j - 1);
+                if (i == 1 && j > 1) {
+                    zar = j - 1;
                 }
-                else
-                {
-                    st=String.valueOf(zar);
+                if (i > 1 && j == 1) {
+                    zar = i - 1;
                 }
-                if(st.length()==1){st=st+"     ";}
-                if(st.length()==2){st=st+"   ";}
-                tv = tv +" "+st;
+                if (i == 1 && j == 1) {
+                    st = "x     ";
+                } else {
+                    st = String.valueOf(zar);
+                }
+                if (st.length() == 1) {
+                    st = st + "     ";
+                }
+                if (st.length() == 2) {
+                    st = st + "   ";
+                }
+                tv = tv + " " + st;
             }
-            tv=tv+"\n\n";
+            tv = tv + "\n\n";
         }
         jad.setText(tv);
 
-        public void sixgreat() {
+        public void sixgreat () {
 
             String tv = "";
             txt.setText(tv);
@@ -58,7 +61,6 @@ TextView jad;
             int min = 200;
             int max = 400;
             int temp = 0;
-
 
 
             for (int i = 0; i <= 5; i++) {
@@ -80,26 +82,25 @@ TextView jad;
                 }
 
 
-
             }
 
             for (int i = 0; i <= 5; i++) {
 
-                st=String.valueOf(rnum[i]);
+                st = String.valueOf(rnum[i]);
 
 
-                tv = tv + " "+st;
+                tv = tv + " " + st;
             }
-            tv=tv +"\n\n";
-            tv=tv+"Last Two Great : " + rnum[0] + " " +rnum[1] ;
-            tv=tv +"\n\n";
-            int sum=rnum[0]  + rnum[1] ;
+            tv = tv + "\n\n";
+            tv = tv + "Last Two Great : " + rnum[0] + " " + rnum[1];
+            tv = tv + "\n\n";
+            int sum = rnum[0] + rnum[1];
 
-            tv=tv+"Sum Is : " + String.valueOf(sum);
+            tv = tv + "Sum Is : " + String.valueOf(sum);
             txt.setText(tv);
 
 
+        }
+
     }
-
-
 }
